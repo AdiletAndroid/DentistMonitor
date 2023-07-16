89a6d9ea57d434b5296cb8bb52764d21b927bad8
dantist987 <adik.isak2412@gmail.com>
2023-07-16 20:42:26 +0600

    Comments edited

--- app/src/main/java/com/example/dentistmonitor/calendar/FireDatabaseHelper.kt
+++ app/src/main/java/com/example/dentistmonitor/calendar/FireDatabaseHelper.kt
@@ -7,22 +7,29 @@ class FireDatabaseHelper {
     private val database: FirebaseDatabase = FirebaseDatabase.getInstance()
     private val rootRef: DatabaseReference = database.reference
 
-    // Метод для добавления нового доктора в базу данных
+    //  добавлениe нового доктора в базу данных
     fun addDoctor(doctorId: String, name: String, calendarId: String) {
         val doctorRef = rootRef.child("doctors").child(doctorId)
         doctorRef.child("name").setValue(name)
         doctorRef.child("calendarId").setValue(calendarId)
     }
 
-    // Метод для добавления нового календаря в базу данных
+    // добавлениe нового календаря в базу данных
     fun addCalendar(calendarId: String, name: String) {
         val calendarRef = rootRef.child("calendars").child(calendarId)
         calendarRef.child("name").setValue(name)
     }
 
-    // Метод для добавления новой записи пациента в календарь
-    fun addAppointment(calendarId: String, appointmentId: String, patientId: String, time: String, additionalData: String) {
-        val appointmentRef = rootRef.child("calendars").child(calendarId).child("appointments").child(appointmentId)
+    // добавлениe новой записи пациента в календарь
+    fun addAppointment(
+        calendarId: String,
+        appointmentId: String,
+        patientId: String,
+        time: String,
+        additionalData: String
+    ) {
+        val appointmentRef =
+            rootRef.child("calendars").child(calendarId).child("appointments").child(appointmentId)
         appointmentRef.child("patientId").setValue(patientId)
         appointmentRef.child("time").setValue(time)
         appointmentRef.child("additionalData").setValue(additionalData)
